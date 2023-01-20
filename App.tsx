2f4da9fdc1656cdc175fa351f8a0065e509c915c
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-20 18:23:25 -0300

    creating navigation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import "./src/lib/dayjs";
+
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -7,8 +8,9 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
+
 import { Loading } from "./src/components/Loading";
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,11 +26,11 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
         translucent
-        barStyle={"light-content"}
-        backgroundColor={"transparent"}
       />
     </>
   );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,38 +1,46 @@
 import { Text, View, ScrollView } from "react-native";
-import { DAY_SIZE, HabitDay } from "../components/HabitDay";
-import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
+
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
-const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+import { Header } from "../components/Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 
-const datesFromYearBeginning = generateDatesFromYearBeginning();
-const minimumSummaryDatesSizes = 18 * 7;
-const amountOfDaysToFill =
-  minimumSummaryDatesSizes - datesFromYearBeginning.length;
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
   return (
-    <View className="flex-1 bg-background px-8  pt-16">
+    <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
       <View className="flex-row mt-6 mb-2">
-        {weekDays.map((weekDay, index) => (
+        {weekDays.map((weekDay, i) => (
           <Text
-            key={`${weekDay}-${index}`}
-            style={{ width: DAY_SIZE }}
+            key={`${weekDay}-${i}`}
             className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
           >
             {weekDay}
           </Text>
         ))}
       </View>
+
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 30 }}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
         <View className="flex-row flex-wrap">
-          {datesFromYearBeginning.map((date) => (
-            <HabitDay key={date.toISOString()} />
+          {datesFromYearStart.map((date) => (
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
+
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
               <View
